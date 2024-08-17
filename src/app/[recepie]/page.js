7a474de9173467c395d5b541fc77cea5a49c289c
ian <ianalvesvalongo2008@gmail.com>
2024-08-17 14:50:03 -0300

    recepie view not found bug

--- src/app/[recepie]/page.js
+++ src/app/[recepie]/page.js
@@ -1,23 +1,9 @@
 import Recepie from "@/comp/recepie";
 
-export async function generateStaticParams() {
-  const response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/search.php?s=",
-  );
-  const data = await response.json();
-
-  if (data.meals) {
-    return data.meals.map((meal) => ({
-      recepie: meal.idMeal.toString(),
-    }));
-  }
-
-  return [];
-}
-
 const RecepiePage = async ({ params }) => {
   const id = params.recepie;
 
+  console.log(params.recepie);
   const response = await fetch(
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
   );
