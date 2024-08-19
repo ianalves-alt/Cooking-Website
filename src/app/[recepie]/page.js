ff5ca3b52a727b03954c37dafc88288f7cb7f662
ian <ianalvesvalongo2008@gmail.com>
2024-08-19 16:38:57 -0300

    bug fix try part 2

--- src/app/[recepie]/page.js
+++ src/app/[recepie]/page.js
@@ -1,5 +1,6 @@
 import Recepie from "@/comp/recepie";
 
+// This function generates static parameters for each recipe page
 export async function generateStaticParams() {
   const categories = [
     "Beef",
@@ -20,14 +21,19 @@ export async function generateStaticParams() {
 
   const allMeals = [];
 
+  // Fetching all meals for each category
   for (const category of categories) {
-    const res = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`,
-    );
-    const data = await res.json();
+    try {
+      const res = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`,
+      );
+      const data = await res.json();
 
-    if (data.meals) {
-      allMeals.push(...data.meals);
+      if (data.meals) {
+        allMeals.push(...data.meals);
+      }
+    } catch (error) {
+      console.error(`Error fetching meals for category ${category}:`, error);
     }
   }
 
@@ -37,13 +43,20 @@ export async function generateStaticParams() {
   }));
 }
 
+// This component fetches and displays the details of a specific recipe
 const RecepiePage = async ({ params }) => {
   const id = params.recepie;
 
-  const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
-  );
-  const data = await response.json();
+  let data;
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
+    );
+    data = await response.json();
+  } catch (error) {
+    console.error(`Error fetching recipe for ID ${id}:`, error);
+    return <div>Error loading recipe.</div>;
+  }
 
   if (!data.meals || data.meals.length === 0) {
     return <div>Recipe not found.</div>;
